7456b731584b24a171df3326102bdbbc1dca5897
Marvin Luchs <marvin.luchs@holisticon.de>
2018-11-14 10:32:31 +0100

    chore: project setup

---
+++ src/main/java/org/hypercontract/hypershop/HypershopApplication.java
@@ -0,0 +1,12 @@
+package org.hypercontract.hypershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypershopApplication.class, args);
+	}
+}
