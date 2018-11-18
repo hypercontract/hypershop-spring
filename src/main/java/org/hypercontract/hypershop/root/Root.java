748259b270eab57536df4a345f6b095c4e01ad89
Marvin Luchs <marvin.luchs@holisticon.de>
2018-11-18 11:12:27 +0100

    feat: implement RootController

---
+++ src/main/java/org/hypercontract/hypershop/root/Root.java
@@ -0,0 +1,12 @@
+package org.hypercontract.hypershop.root;
+
+import lombok.*;
+
+@NoArgsConstructor
+@ToString
+public class Root {
+
+    @Getter
+    private final String version = "1.0";
+
+}
---
+++ src/main/java/org/hypercontract/hypershop/root/RootController.java
@@ -0,0 +1,16 @@
+package org.hypercontract.hypershop.root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public Root get() {
+        return new Root();
+    }
+
+}
