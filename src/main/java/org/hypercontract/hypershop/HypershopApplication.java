6ade395adea099f0c50999f3b8e6a152c4baa305
Marvin Luchs <marvin@luchs.org>
2018-12-26 17:02:11 +0100

    fix: serialize lazy-loaded property values

--- src/main/java/org/hypercontract/hypershop/HypershopApplication.java
+++ src/main/java/org/hypercontract/hypershop/HypershopApplication.java
@@ -1,5 +1,7 @@
 package org.hypercontract.hypershop;
 
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.hypercontract.hypershop.mock.MockData;
 import org.hypercontract.hypershop.mock.MockDataFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,4 +19,11 @@ public class HypershopApplication {
     public MockData mockData(MockDataFactory mockDataFactory) {
 	    return mockDataFactory.createMockData();
     }
+
+	@Bean
+	protected Module jacksonHibernateModule() {
+		var module = new Hibernate5Module();
+		module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return module;
+	}
 }
