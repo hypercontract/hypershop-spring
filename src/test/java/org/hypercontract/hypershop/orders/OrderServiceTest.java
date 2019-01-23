aacc8802baadbfe048f3512579eb3fe9503acc00
Marvin Luchs <marvin@luchs.org>
2019-01-23 21:10:12 +0100

    test: add some tests for OrderService

---
+++ src/test/java/org/hypercontract/hypershop/orders/OrderServiceTest.java
@@ -0,0 +1,45 @@
+package org.hypercontract.hypershop.orders;
+
+import org.hypercontract.hypershop.resource.Id;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OrderServiceTest {
+
+    @InjectMocks
+    private OrderService orderService;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @Test
+    public void getByIdReturnsAnOrder() {
+        var expectedId = new Id<Order>("something");
+        var expectedOrder = Order.builder().id(expectedId).build();
+        when(orderRepository.findById(expectedId)).thenReturn(Optional.of(expectedOrder));
+
+        var order = orderService.getById(expectedId);
+
+        assertSame(order, expectedOrder);
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void getByIdThrowsIfIdIsInvalid() {
+        when(orderRepository.findById(any())).thenReturn(Optional.empty());
+
+        orderService.getById(new Id<>());
+    }
+
+}
