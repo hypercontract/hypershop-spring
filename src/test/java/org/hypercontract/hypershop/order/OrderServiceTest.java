94b0d598faefdc9364873e8058aed7db9a5208ee
Marvin Luchs <marvin.luchs@holisticon.de>
2019-08-02 14:47:36 +0200

    test: add OrderService tests

--- src/test/java/org/hypercontract/hypershop/order/OrderServiceTest.java
+++ src/test/java/org/hypercontract/hypershop/order/OrderServiceTest.java
@@ -1,20 +1,34 @@
 package org.hypercontract.hypershop.order;
 
+import org.hypercontract.hypershop.order.mock.OrderTestData;
 import org.hypercontract.hypershop.resource.Id;
+import org.hypercontract.hypershop.shoppingCart.ShoppingCartItem;
+import org.hypercontract.hypershop.shoppingCart.mock.ShoppingCartItemTestData;
+import org.hypercontract.hypershop.userProfile.Address;
+import org.hypercontract.hypershop.userProfile.PaymentOption;
+import org.hypercontract.hypershop.userProfile.mock.AddressTestData;
+import org.hypercontract.hypershop.userProfile.mock.PaymentOptionTestData;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.persistence.EntityNotFoundException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.hypercontract.hypershop.order.OrderStatus.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class OrderServiceTest {
 
     @InjectMocks
@@ -23,10 +37,18 @@ public class OrderServiceTest {
     @Mock
     private OrderRepository orderRepository;
 
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    private final OrderTestData orderTestData = OrderTestData.getInstance();
+    private final ShoppingCartItemTestData shoppingCartItemTestData = ShoppingCartItemTestData.getInstance();
+    private final AddressTestData addressTestData = AddressTestData.getInstance();
+    private final PaymentOptionTestData paymentOptionTestData = PaymentOptionTestData.getInstance();
+
     @Test
     public void getByIdReturnsAnOrder() {
-        var expectedId = new Id<Order>("something");
-        var expectedOrder = Order.builder().id(expectedId).build();
+        var expectedOrder = orderTestData.getOrder();
+        var expectedId = expectedOrder.getId();
         when(orderRepository.findById(expectedId)).thenReturn(Optional.of(expectedOrder));
 
         var order = orderService.getById(expectedId);
@@ -39,6 +61,107 @@ public class OrderServiceTest {
         when(orderRepository.findById(any())).thenReturn(Optional.empty());
 
         orderService.getById(new Id<>());
+    }
+
+    @Test
+    public void findAllReturnsAllOrders() {
+        var expectedOrders = orderTestData.getOrders();
+        when(orderRepository.findAll()).thenReturn(expectedOrders.stream());
+
+        var orders = orderService.findAll();
+
+        assertEquals(orders, expectedOrders);
+    }
+
+    @Test
+    public void placeOrderCreatesANewOrder() {
+        var shoppingCartItems = shoppingCartItemTestData.getShoppingCartItems();
+        var billingAddress = addressTestData.getAddress();
+        var shippingAddress = addressTestData.getAnotherAddress();
+        var paymentOption = paymentOptionTestData.getPaymentOption();
+        var orderCaptor = ArgumentCaptor.forClass(Order.class);
+
+        orderService.placeOrder(
+            shoppingCartItems,
+            billingAddress,
+            shippingAddress,
+            paymentOption
+        );
+
+        verify(orderRepository).save(orderCaptor.capture());
+
+        var savedOrder = orderCaptor.getValue();
+        assertTrue(Duration.between(savedOrder.getDate(), LocalDateTime.now()).toMinutes() < 1);
+        assertSame(savedOrder.getStatus(), OrderStatus.PAYMENT_DUE);
+        assertEqualsAddress(savedOrder.getBillingAddress(), billingAddress);
+        assertEqualsAddress(savedOrder.getShippingAddress(), shippingAddress);
+        assertEqualsPaymentOption(savedOrder.getPayment(), paymentOption);
+        assertEqualsShoppingCartItems(savedOrder.getItems(), shoppingCartItems);
+    }
+
+    @Test
+    public void placeOrderPublishesAnOrderPlacedEvent() {
+        orderService.placeOrder(
+            shoppingCartItemTestData.getShoppingCartItems(),
+            addressTestData.getAddress(),
+            addressTestData.getAnotherAddress(),
+            paymentOptionTestData.getPaymentOption()
+        );
+
+        verify(eventPublisher).publishEvent(any(OrderPlaced.class));
+    }
+
+    @Test
+    public void cancelOrder() {
+        var order = orderTestData.getOrder();
+        when(orderRepository.findById(order.getId())).thenReturn(Optional.of(order));
+
+        orderService.cancelOrder(order.getId());
+
+        verify(orderRepository).save(order);
+        assertEquals(order.getStatus(), CANCELLED);
+    }
+
+    @Test
+    public void returnOrder() {
+        var order = orderTestData.getOrder();
+        when(orderRepository.findById(order.getId())).thenReturn(Optional.of(order));
+
+        orderService.returnOrder(order.getId());
+
+        verify(orderRepository).save(order);
+        assertEquals(order.getStatus(), RETURNED);
+    }
+
+    private void assertEqualsAddress(OrderAddress orderAddress, Address address) {
+        assertEquals(address.getName(), orderAddress.getName());
+        assertEquals(address.getCity(), orderAddress.getCity());
+        assertEquals(address.getZipCode(), orderAddress.getZipCode());
+        assertEquals(address.getCity(), orderAddress.getCity());
+        assertEquals(address.getCountry(), orderAddress.getCountry());
+    }
+
+    private void assertEqualsPaymentOption(OrderPayment orderPayment, PaymentOption paymentOption) {
+        assertEquals(paymentOption.getAccountOwner(), orderPayment.getAccountOwner());
+        assertEquals(paymentOption.getIban(), orderPayment.getIban());
+        assertEquals(paymentOption.getBic(), orderPayment.getBic());
+    }
+
+    private void assertEqualsShoppingCartItems(List<OrderItem> orderItems, List<ShoppingCartItem> shoppingCartItems) {
+        assertEquals(orderItems.size(), shoppingCartItems.size());
+
+        shoppingCartItems.forEach(shoppingCartItem -> {
+            var orderItem = orderItems.stream()
+                .filter(item -> item.getProduct().equals(shoppingCartItem.getProduct()))
+                .findAny();
+
+            orderItem.ifPresent(savedOrderItem -> {
+                assertEquals(savedOrderItem.getDescription(), shoppingCartItem.getDescription());
+                assertEquals(savedOrderItem.getName(), shoppingCartItem.getName());
+                assertEquals(savedOrderItem.getPrice(), shoppingCartItem.getPrice());
+                assertEquals(savedOrderItem.getQuantity(), shoppingCartItem.getQuantity());
+            });
+        });
     }
 
 }
